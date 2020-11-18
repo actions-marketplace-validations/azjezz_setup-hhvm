a644b0770428e1889b5d03bbf60d2b2418128e1d
azjezz <azjezz@protonmail.com>
2020-11-18 02:34:59 +0100

    -

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -6,10 +6,14 @@ EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
-if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]; then
-  >&2 echo 'ERROR: Invalid installer checksum'
-  rm composer-setup.php
-  exit 1
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
 fi
 
-exit $(php composer-setup.php --install-dir=/usr/local/bin --filename=composer) && $(rm composer-setup.php)
+php composer-setup.php "$@"
+RESULT=$?
+rm composer-setup.php
+exit $RESULT
