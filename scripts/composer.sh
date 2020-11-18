1d943bfe27955e667a91cd38e3c4e5bfffada7e8
azjezz <azjezz@protonmail.com>
2020-11-18 04:18:58 +0100

    fix composer installation

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -1,19 +1,15 @@
-#!/bin/sh
-
-# From https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
-
 EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
-
-if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
-then
-    >&2 echo 'ERROR: Invalid installer checksum'
-    rm composer-setup.php
-    exit 1
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]; then
+  >&2 echo 'ERROR: Invalid installer checksum'
+  rm composer-setup.php
+  exit 1
+else
+  mkdir -p $HOME/bin
+  php composer-setup.php --install-dir="$HOME/bin" --filename=composer
+  echo "$HOME/bin" > $GITHUB_PATH
+  RESULT=$?
+  rm composer-setup.php
+  exit $RESULT
 fi
-
-php composer-setup.php "$@"
-RESULT=$?
-rm composer-setup.php
-exit $RESULT
